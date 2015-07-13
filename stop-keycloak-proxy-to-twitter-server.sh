37ad62b004d03a569e9adc2baed5676cf9b98194
Michael Martin <mwm.cal@gmail.com>
2015-07-13 17:59:13 -0600

    fixing a few things

--- stop-keycloak-proxy-to-twitter-server.sh
+++ stop-keycloak-proxy-to-twitter-server.sh
@@ -5,6 +5,7 @@ fleetctl stop twitter-http-discovery
 fleetctl stop twitter-http
 
 fleetctl destroy keycloak-proxy-to-twitter-http
+fleetctl destroy twitter-http-discovery
 fleetctl destroy twitter-http
 
 fleetctl list-units
